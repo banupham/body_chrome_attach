7b39d5e212e9313b1513e207eca6fd8a7676d336
banupham <135731801+banupham@users.noreply.github.com>
2026-09-06 22:25:11 +0700

    feat: launch daemon with protection guardian

--- daemon.cmd
+++ daemon.cmd
@@ -1,2 +1,2 @@
 @echo off
-node -r "%~dp0daemon\src\sticky_runtime_port_preload.js" "%~dp0daemon\server.js"
+node -r "%~dp0daemon\src\sticky_runtime_port_preload.js" "%~dp0daemon\guardian_bootstrap.js"
