a35dfcbe87494a7a523b556a0419d8c1ca68461c
banupham <135731801+banupham@users.noreply.github.com>
2026-09-07 00:02:07 +0700

    fix: simplify Windows debug CLI wrapper

--- body.cmd
+++ body.cmd
@@ -1,10 +1,3 @@
 @echo off
-setlocal
-set "BODY_CLI=%~dp0body_cli.js"
-if not exist "%BODY_CLI%" (
-  echo [LOI] Khong tim thay "%BODY_CLI%" 1>&2
-  endlocal & exit /b 2
-)
-node "%BODY_CLI%" %*
-set "BODY_RC=%ERRORLEVEL%"
-endlocal & exit /b %BODY_RC%
+node "%~dp0body_cli.js" %*
+exit /b %ERRORLEVEL%
