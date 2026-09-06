b6c6b0c52049aefc7c69f68e4654b416eb8f13b7
banupham <135731801+banupham@users.noreply.github.com>
2026-09-06 23:52:40 +0700

    Harden Windows body CLI wrapper

--- body.cmd
+++ body.cmd
@@ -1,2 +1,10 @@
 @echo off
-node "%~dp0body_cli.js" %*
+setlocal
+set "BODY_CLI=%~dp0body_cli.js"
+if not exist "%BODY_CLI%" (
+  echo [LOI] Khong tim thay "%BODY_CLI%" 1>&2
+  endlocal & exit /b 2
+)
+node "%BODY_CLI%" %*
+set "BODY_RC=%ERRORLEVEL%"
+endlocal & exit /b %BODY_RC%
