936e2f77617734bda0c15495be31ee33a77382a4
banupham <135731801+banupham@users.noreply.github.com>
2026-09-06 21:57:25 +0700

    test: expose sticky WebSocket preload installer

--- daemon/src/sticky_runtime_port_preload.js
+++ daemon/src/sticky_runtime_port_preload.js
@@ -4,27 +4,34 @@ const path = require('node:path');
 const ws = require('ws');
 const { RUNTIME_HOST, preferredRuntimePort } = require('./runtime_endpoint');
 
-const baseDir = path.resolve(__dirname, '..');
-const rememberedPort = preferredRuntimePort(baseDir);
-
-if (rememberedPort) {
-  const OriginalWebSocketServer = ws.WebSocketServer;
+function installStickyRuntimePort(wsModule, rememberedPort) {
+  const port = Number(rememberedPort);
+  if (!Number.isInteger(port) || port < 1 || port > 65535) return false;
+  const OriginalWebSocketServer = wsModule?.WebSocketServer;
+  if (typeof OriginalWebSocketServer !== 'function') throw new Error('websocket_server_constructor_unavailable');
 
   class StickyRuntimeWebSocketServer extends OriginalWebSocketServer {
     constructor(options = {}, callback) {
       const shouldReuse = options?.host === RUNTIME_HOST && Number(options?.port) === 0;
-      const nextOptions = shouldReuse ? { ...options, port: rememberedPort } : options;
+      const nextOptions = shouldReuse ? { ...options, port } : options;
       super(nextOptions, callback);
       if (shouldReuse) {
         this.bodyRuntimePortMode = 'remembered_auto_port';
-        this.bodyRuntimeRequestedPort = rememberedPort;
+        this.bodyRuntimeRequestedPort = port;
       }
     }
   }
 
-  ws.WebSocketServer = StickyRuntimeWebSocketServer;
-  if (ws.Server === OriginalWebSocketServer) ws.Server = StickyRuntimeWebSocketServer;
+  wsModule.WebSocketServer = StickyRuntimeWebSocketServer;
+  if (wsModule.Server === OriginalWebSocketServer) wsModule.Server = StickyRuntimeWebSocketServer;
+  return true;
+}
+
+const baseDir = path.resolve(__dirname, '..');
+const rememberedPort = preferredRuntimePort(baseDir);
+if (rememberedPort) {
+  installStickyRuntimePort(ws, rememberedPort);
   process.env.BODY_RUNTIME_REMEMBERED_PORT = String(rememberedPort);
 }
 
-module.exports = { rememberedPort };
+module.exports = { rememberedPort, installStickyRuntimePort };
